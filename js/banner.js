ab8e923c3ebb935174c92cc742e62c30dcd58c57
devwjdalsdl <devwjdalsdl07@gmail.com>
2023-05-23 21:08:42 +0900

    feat : 기본 html 섹션 구조 분석 적용 및 css, js 파일 구성
    html Section 와이어 프레임 적용
    css 파일 분리
    js 파일 분리

---
+++ js/banner.js
@@ -0,0 +1,12 @@
+// 모든 리소스 로딩 후 실행
+/**
+ * 작업자 : 손정민
+ * 작업일 : 2023-05-23
+ * 작업내역 :배너 작업
+ */
+// event.currentTarget : 누가 발생시켰는지(대상)
+// event.target : 누가 이벤트를 처리하는가
+// "load" : 이벤트 타입(종류)
+// 콜백함수(function(){}) : 이벤트 핸들러
+// 매개변수 : event 는 이벤트 객체가 암묵적으로 전달해 주는 인자(arguments)다. 
+window.addEventListener("load",function(event) {});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 // 모든 리소스 로딩 후 실행
-window.onload = function () {
+/**
+ * 작업자 : 손정민
+ * 작업일 : 2023-05-23
+ * 작업내역 :기본 환경설정
+ */
+window.addEventListener("load", function(event) {});
     
-};
+
